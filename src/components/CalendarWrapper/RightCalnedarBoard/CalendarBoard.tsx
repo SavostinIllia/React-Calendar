b661cb373565d1a39412ba06b29bf0532880d0d0
SavostinIllia <skyshop@MacBook-Air-skyshop.local>
2024-12-04 13:51:14 +0100

    dep

--- src/components/CalendarWrapper/RightCalnedarBoard/CalendarBoard.tsx
+++ src/components/CalendarWrapper/RightCalnedarBoard/CalendarBoard.tsx
@@ -1,5 +1,5 @@
 
-import React , {useState}from "react";
+import React from "react";
 import { CreateDateReturnType, Holiday } from "../../../types"
 import { CalendarDayTaskSetter } from "./CalendarDayTaskSetter";
 import { useCalendarDayTasksContext } from "../../../context";
